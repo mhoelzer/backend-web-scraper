fd27c8475a25318c0bbf34cb1d9ad23d26f8d138
Michelle Hoelzer <hoelzermm@gmail.com>
2019-03-19 14:51:21 -0400

    new format established; nothing really working yet

--- web_scraper.py
+++ web_scraper.py
@@ -7,13 +7,26 @@ import argparse
 import requests
 import re
 import sys
+from bs4 import BeautifulSoup
 
 
 def scraper(url):
     """"""
     response = requests.get(url)
-    for url in response:
-        pass
+    souped_url = BeautifulSoup(response.text, 'html.parser')
+    return souped_url
+
+
+def find_urls(souped_url):
+    pass
+
+
+def find_emails(souped_url):
+    pass
+
+
+def find_phone_numbers(souped_url):
+    pass
 
 
 def create_parser():
@@ -31,9 +44,14 @@ def main(args):
         sys.exit(1)
     namespace = parser.parse_args(args)
     url_to_be_scraped = namespace.url
-    return scraper(url_to_be_scraped)
+    scraped_to_be_souped = scraper(url_to_be_scraped)
+    if scraped_to_be_souped:
+        find_urls(scraped_to_be_souped)
+        find_emails(scraped_to_be_souped)
+        find_phone_numbers(scraped_to_be_souped)
 
 
 if __name__ == "__main__":
     # example of cmdl: python scraper.py https://nookpaleo.com
-    print(main(sys.argv[1:]))
+    # print(main(sys.argv[1:]))
+    main()
